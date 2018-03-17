0f079d1fc99216a7aaed44468d9268704e07ad9e
AcerHack <acerhack@AcerHacks-MacBook-Air.local>
2018-03-17 21:52:15 +0300

    Initial Commit

---
+++ Furniture/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Furniture
+//
+//  Created by AcerHack on 17.03.18.
+//  Copyright © 2018 AcerHack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
